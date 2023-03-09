3194da2c4447bace88ea89a22d88de1fb4a8374e
Matheus Damasceno <matheusfjxj@gmail.com>
2023-03-08 22:43:34 -0300

    começando

--- Funcao/cidadaoPrimeiraClasse.js
+++ Funcao/cidadaoPrimeiraClasse.js
@@ -5,4 +5,24 @@ function fun1(){}
 const fun2 = function () {}
 
 //Armazenar em um array
-const fun3 = [fun1(),fun2()]
+const array = [function (a,b){return a + b},fun1(),fun2()]
+console.log(array[0](5,4))
+
+//Armazenar em um objeto
+const obj = {}
+obj.fun3 = function () {return "ola mundo"}
+console.log(obj.fun3())
+
+//Usar de parametro
+function run(fun){
+    fun()
+}
+run(function (){console.log('oiii')})
+
+//pode retornar/ober funcao
+function soma(a,b){
+    return function (c){
+        console.log(a+b+c)
+    }
+}
+soma(3,4)(5)
